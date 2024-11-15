e5f396a9ec75079fa10cc384da5623cc4de39a14
Shaghayegh9473 <universeplanet2010@gmail.com>
2024-11-16 00:41:33 +0800

    Added Bootstrap, form and logo
    Added the fundamental elements and info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header>
+          <a
+            href="https://www.shecodes.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./logo.png" alt="logo" />
+          </a>
+        </header>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            Shaghayegh K.
+          </a>{" "}
+          and is{" "}
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
