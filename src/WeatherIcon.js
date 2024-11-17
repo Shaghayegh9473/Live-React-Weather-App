98620546ef4aea339567d00a3ffe684b51126d6e
Shaghayegh9473 <universeplanet2010@gmail.com>
2024-11-18 04:11:18 +0800

    Added weather icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { WeatherSvg } from "weather-icons-animated";
+import "./Weather.css";
 
 export default function WeatherIcon(props) {
-  const codeMapping = {};
+  const codeMapping = {
+    Clear: "day-sunny",
+    Clouds: "cloudy",
+    Rain: "rain",
+    Snow: "snow",
+    Thunderstorm: "thunderstorm",
+    Drizzle: "sprinkle",
+  };
 
   return <WeatherSvg state={codeMapping[props.code]} width={50} height={50} />;
 }
